00f9b910ecd51c3c71d8d2a774df267fc3f5aa4f
Alexey <93317676+SheeetFace@users.noreply.github.com>
2023-12-12 11:58:15 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -139,54 +139,46 @@ const App: React.FC = () => {
   }
 
   return (
-    // <div className='App'>
+    <div className='App'>
      
 
-    // <div className='App-rules'>
-    //   {status ==='IDLE' ? 
-    //   <>
-    //       <h1>ВАЖНЫЕ ПРАВИЛА ДЛЯ КОРРЕКТНОЙ РАБОТЫ</h1>
-    //       <h3>👉🏻1. формат документа должен быть .xlsx</h3>
+    <div className='App-rules'>
+      {status ==='IDLE' ? 
+      <>
+          <h1>ВАЖНЫЕ ПРАВИЛА ДЛЯ КОРРЕКТНОЙ РАБОТЫ</h1>
+          <h3>👉🏻1. формат документа должен быть .xlsx</h3>
 
-    //       <h3>👉🏻2. Документ НЕ должен содержать лишних записей выше или ниже, только сама таблица </h3>
-    //       <div>
-    //         <div className='App-rules-imageBOx'>
-    //           <div>
-    //             <h4 style={{color:"red", fontSize:'2rem'}}>НЕ ВЕРНО</h4>
-    //             <img  src='https://cdn.discordapp.com/attachments/872343092500504628/1133728142620635236/image.png' alt=''></img>
-    //           </div>
+          <h3>👉🏻2. Документ НЕ должен содержать лишних записей выше или ниже, только сама таблица </h3>
+          <div>
+            <div className='App-rules-imageBOx'>
+              <div>
+                <h4 style={{color:"red", fontSize:'2rem'}}>НЕ ВЕРНО</h4>
+                <img  src='https://cdn.discordapp.com/attachments/872343092500504628/1133728142620635236/image.png' alt=''></img>
+              </div>
 
-    //           <div>
-    //             <h4 style={{color:"green",fontSize:'2rem'}}>ВЕРНО</h4>
-    //             <img src='https://cdn.discordapp.com/attachments/872343092500504628/1133726399694372905/image.png' alt=''></img>
-    //           </div>
-    //         </div>
-    //       </div>
-    //       <h3>👉🏻3. ОБЯЗАТЕЛЬНО В НАЗВАНИЯХ ТАБЛИЦЫ С ПРИХОДОМ ДОЛЖНО БЫТЬ КЛЮЧЕВОЕ СЛОВО 👉🏻[Вход]👈🏻, что перед ним или после него не имеет значения. Этого же качается и 👉🏻[Выход]👈🏻  </h3>
-    //       <h3>👉🏻4. [Вход] с форматов времени такого типа 25.07.2023 07:32 - НЕ БУДЕТ ПОСЧИТАН, только с таким форматом 07:32 </h3>
-    //       <h3>👉🏻5. После загрузки файла, сразу же будет скачен новый файл с названием "отчет.xlsx" 
-    //       В  Новом файле вместо названия столбца 24.07.2023 Понедельник[Присутствие] будет просто "Присутствие"
-    //       </h3>
-    //   </>
-    //   :
-    //   <div className='success'>
-    //     ФАЙЛ СКАЧЕН
-    //   </div>
-    //   }
-    // </div>
-    // <h3 style={{color:"white",fontSize:'2rem', textAlign:"center"}}>Итог такой, просто убрать шапку (пункт 2) и должно работать </h3>
-    // <div className='App-box'>
-    //     <input className='App-box-button' type="file" onChange={handleFileUpload} />
-    //   </div>
-    // </div>
-        <iframe
-        
-          src="https://games.wavey.group/games/doom2/index.html"
-          width="99.5%"
-          height="700px"
-          allow="autoplay"
-        >
-        </iframe>
+              <div>
+                <h4 style={{color:"green",fontSize:'2rem'}}>ВЕРНО</h4>
+                <img src='https://cdn.discordapp.com/attachments/872343092500504628/1133726399694372905/image.png' alt=''></img>
+              </div>
+            </div>
+          </div>
+          <h3>👉🏻3. ОБЯЗАТЕЛЬНО В НАЗВАНИЯХ ТАБЛИЦЫ С ПРИХОДОМ ДОЛЖНО БЫТЬ КЛЮЧЕВОЕ СЛОВО 👉🏻[Вход]👈🏻, что перед ним или после него не имеет значения. Этого же качается и 👉🏻[Выход]👈🏻  </h3>
+          <h3>👉🏻4. [Вход] с форматов времени такого типа 25.07.2023 07:32 - НЕ БУДЕТ ПОСЧИТАН, только с таким форматом 07:32 </h3>
+          <h3>👉🏻5. После загрузки файла, сразу же будет скачен новый файл с названием "отчет.xlsx" 
+          В  Новом файле вместо названия столбца 24.07.2023 Понедельник[Присутствие] будет просто "Присутствие"
+          </h3>
+      </>
+      :
+      <div className='success'>
+        ФАЙЛ СКАЧЕН
+      </div>
+      }
+    </div>
+    <h3 style={{color:"white",fontSize:'2rem', textAlign:"center"}}>Итог такой, просто убрать шапку (пункт 2) и должно работать </h3>
+    <div className='App-box'>
+        <input className='App-box-button' type="file" onChange={handleFileUpload} />
+      </div>
+    </div>
   );
 }
 
